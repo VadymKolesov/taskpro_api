29358b1e2f811d6bce7a111847bdeee2db4a61fe
Svitlana <wsv3ta@gmail.com>
2024-06-05 22:00:22 +0200

    add auth schemas

---
+++ schemas/authSchemas.js
@@ -0,0 +1,28 @@
+import Joi from 'joi';
+
+const registerUserSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().pattern(new RegExp('^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$')).required(),
+    password: Joi.string().min(8).required()
+});
+
+const loginUserSchema = Joi.object({
+    email: Joi.string().pattern(new RegExp('^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$')).required(),
+    password: Joi.string().min(8).required()
+});
+
+const updateUserSchema = Joi.object({
+    name: Joi.string(),
+    email: Joi.string().pattern(new RegExp('^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$')),
+    password: Joi.string().min(8)
+}).min(1);
+
+const updateAvatarSchema = Joi.object({
+    avatarUrl: Joi.string().required()
+});
+
+const updateThemeSchema = Joi.object({
+    theme: Joi.string().valid("dark", "light", "violet").required()
+});
+
+export default { registerUserSchema, loginUserSchema, updateUserSchema, updateAvatarSchema, updateThemeSchema }
