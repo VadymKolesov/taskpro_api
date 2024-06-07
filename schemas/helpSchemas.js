5066e0f93d83e229567460032d2679014013264f
NovichokUA <zubr7333@gmail.com>
2024-06-07 21:04:26 +0300

    regex for email

--- schemas/helpSchemas.js
+++ schemas/helpSchemas.js
@@ -1,7 +1,12 @@
 import Joi from "joi";
 
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
 const needHelp = Joi.object({
-  email: Joi.email().required(),
+  email: Joi.string().pattern(emailRegex).required().messages({
+    "string.pattern.base": "Invalid email format",
+    "any.required": "Email is required",
+  }),
   comment: Joi.string().max(100).required().messages({
     "string.min": "Comment must contain less than 100 characters",
     "any.required": "Comment is required",
