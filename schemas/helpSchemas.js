9c3808a2b1ff8fd1629204b4733a5ebadbbe18d2
NovichokUA <zubr7333@gmail.com>
2024-06-07 21:00:53 +0300

    change mess

--- schemas/helpSchemas.js
+++ schemas/helpSchemas.js
@@ -2,7 +2,10 @@ import Joi from "joi";
 
 const needHelp = Joi.object({
   email: Joi.email().required(),
-  comment: Joi.string().max(100).required(),
+  comment: Joi.string().max(100).required().messages({
+    "string.min": "Comment must contain less than 100 characters",
+    "any.required": "Comment is required",
+  }),
 });
 
 export default { needHelp };
