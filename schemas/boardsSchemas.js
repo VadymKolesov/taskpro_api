5a44cc929fcfa5852df8d9d25cbcc12c94a6b0f6
Vadym <46047631+VadymKolesov@users.noreply.github.com>
2024-06-07 15:24:43 +0200

    edits boards schemas

--- schemas/boardsSchemas.js
+++ schemas/boardsSchemas.js
@@ -1,47 +1,14 @@
 import Joi from "joi";
 
-const newBoard = Joi.object({
-  title: Joi.string()
-    .max(30)
-    .required()
-    .label("Title")
-    .message("Title is required."),
-  iconName: Joi.string().required(),
-  backgroundUrl: Joi.string().required(),
+export const boardSchema = Joi.object({
+  title: Joi.string().max(18).required().messages({
+    "string.min": "Title must contain less than 18 characters",
+    "any.required": "Title is required",
+  }),
+  iconName: Joi.string().required().messages({
+    "any.required": "Icon name is required",
+  }),
+  backgroundUrl: Joi.string().required().messages({
+    "any.required": "Backgound url is required",
+  }),
 });
-
-const editBoard = Joi.object({
-  title: Joi.string().max(30).label("Title"),
-  iconName: Joi.string(),
-  backgroundUrl: Joi.string(),
-}).min(1);
-
-// const newCard = Joi.object({
-//   title: Joi.string().max(30).required().label("Title").messages({
-//     "any.required": "Title is required.",
-//   }),
-//   description: Joi.string().max(250).required().label("Description").messages({
-//     "any.required": "Description is required.",
-//   }),
-//   priority: Joi.string().valid("low", "medium", "high").required(),
-//   category: Joi.string().valid("to-do", "in-progress", "done").required(),
-//   dateDeadline: Joi.string().regex(DATE_REGEXP).required().messages({
-//     "any.required": "Date is required.",
-//   }),
-// });
-
-// const editCard = Joi.object({
-//   title: Joi.string().max(100),
-//   description: Joi.string().max(250),
-//   priority: Joi.string().valid("low", "medium", "high"),
-//   category: Joi.string().valid("to-do", "in-progress", "done"),
-//   dateDeadline: Joi.string().regex(DATE_REGEXP),
-// });
-
-// const newColumn = Joi.object({
-//   title: Joi.string().max(100).required().label("Title").messages({
-//     "any.required": "Title is required.",
-//   }),
-// }); // приміняти ї для editColumn
-
-export default { newBoard, editBoard };
