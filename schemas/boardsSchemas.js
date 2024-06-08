dd9a34d7b856f4a3555c88cb3ee068eaa53c8834
Vadym <46047631+VadymKolesov@users.noreply.github.com>
2024-06-08 23:00:22 +0200

    add new routes

--- schemas/boardsSchemas.js
+++ schemas/boardsSchemas.js
@@ -1,11 +1,26 @@
 import Joi from "joi";
-import objectIdValidator from "../helpers/objectIdValidator.js"
+import objectIdValidator from "../helpers/objectIdValidator.js";
+
+const icons = [
+  "icon-project-1",
+  "icon-project-2",
+  "icon-project-3",
+  "icon-project-4",
+  "icon-project-5",
+  "icon-project-6",
+  "icon-project-7",
+  "icon-project-8",
+];
 
 export const boardSchema = Joi.object({
-  name: Joi.string().max(18).required().messages({
-    "string.min": "Name must contain less than 18 characters",
-    "any.required": "Name is required",
-  }),
+  name: Joi.string()
+    .max(18)
+    .valid(...icons)
+    .required()
+    .messages({
+      "string.min": "Name must contain less than 18 characters",
+      "any.required": "Name is required",
+    }),
   iconName: Joi.string().required().messages({
     "any.required": "Icon name is required",
   }),
@@ -18,7 +33,7 @@ export const columnSchema = Joi.object({
   name: Joi.string().max(30).required().messages({
     "string.min": "Name must contain less than 30 characters",
     "any.required": "Name is required",
-  })
+  }),
 });
 
 export const cardSchema = Joi.object({
@@ -33,17 +48,20 @@ export const cardSchema = Joi.object({
   deadline: Joi.string().required().messages({
     "any.required": "Deadline is required",
   }),
-  priority: Joi.string().required().valid("without", "low", "medium", "high").messages({
-    "any.required": "Priority is required",
-  })
+  priority: Joi.string()
+    .required()
+    .valid("without", "low", "medium", "high")
+    .messages({
+      "any.required": "Priority is required",
+    }),
 });
 
 export const cardStatusSchema = Joi.object({
   isDone: Joi.boolean().required().messages({
-    "any.required": "Status is required"
-  })
+    "any.required": "Status is required",
+  }),
 });
 
 export const idSchema = Joi.object({
-  columnId: Joi.string().custom(objectIdValidator).required()
-})
+  columnId: Joi.string().custom(objectIdValidator).required(),
+});
